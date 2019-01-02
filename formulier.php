8ecff973b7e8ba7686906b624d8a2cd261f29fed
RomanWiley <coenena21@gmail.com>
2019-01-02 21:50:19 +0100

    new repository created

---
+++ formulier.php
@@ -0,0 +1,49 @@
+<html>
+
+<title>Inschrijfformulier ZZV'92</title>
+
+<head>
+
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+ </head>
+
+<body>
+
+<div>Inschrijfformulier ZZV'92</div>
+
+<br>
+
+<form action="welcome.php" method="post">
+
+Name: <input type="text" name="name"><br>
+
+E-mail: <input type="text" name="email"><br>
+
+Geboortedatum: <input type="text" name="gebdatum"><br>
+<br>
+
+Geslacht:
+<input type="radio" name="geslacht" value="vrouw">Vrouw
+<input type="radio" name="geslacht" value="man">Man
+<input type="radio" name="geslacht" value="anders">Anders
+
+<br><br>
+
+<input type="submit" style="size: 20%; color: white; background: darkblue;">
+
+</form>
+
+        
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+
+</body>
+
+
+</html> 
