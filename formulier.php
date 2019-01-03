87106d747a0b60245366f2aa8e5fd7051e34376a
CodeGorilla <you@example.com>
2019-01-03 12:27:56 +0100

    gender function added

--- formulier.php
+++ formulier.php
@@ -5,8 +5,7 @@
 <head>
 
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    
+
  </head>
 
 <body>
@@ -14,33 +13,24 @@
 <div>Inschrijfformulier ZZV'92</div>
 
 <br>
+<br>
+<br>
 
 <form action="welcome.php" method="post">
 
-Name: <input type="text" name="name"><br>
-
-E-mail: <input type="text" name="email"><br>
-
-Geboortedatum: <input type="text" name="gebdatum"><br>
+Leeftijd: <input type="text" name="leeftijd"><br>
+<br>
 <br>
 
-Geslacht:
-<input type="radio" name="geslacht" value="vrouw">Vrouw
-<input type="radio" name="geslacht" value="man">Man
-<input type="radio" name="geslacht" value="anders">Anders
+  <input type="radio" name="geslacht" value="man" checked> Man<br>
+  <input type="radio" name="geslacht" value="vrouw"> Vrouw<br>
+  <input type="radio" name="geslacht" value="anders"> Anders<br><br>
+<br>
+<br>
 
-<br><br>
-
-<input type="submit" style="size: 20%; color: white; background: darkblue;">
+<input type="submit" >
 
 </form>
-
-        
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
 
 </body>
