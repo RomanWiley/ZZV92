b9b9d5e5f8c77e430cf90e7b818f3be459780285
Alex Coenen <coenena21@gmail.com>
2019-01-04 09:36:11 +0100

    formvalidation added

--- formulier.php
+++ formulier.php
@@ -9,20 +9,71 @@
  </head>
 
 <body>
+<?php
+// define variables and set to empty values
+$fnameErr = $lnameErr = $emailErr = $genderErr = "";
+$fname = $lname= $email = $geslacht = "";
 
-<div>Inschrijfformulier ZZV'92</div>
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["voornaam"])) {
+    $fnameErr = "Voornaam is verplicht";
+  } else {
+    $fname = test_input($_POST["voornaam"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$fname)) {
+      $fnameErr = "Alleen letters en spaties toegestaan";
+    }
+  }
 
+    if (empty($_POST["achternaam"])) {
+      $lnameErr = "Achternaam is verplicht";
+    } else {
+      $lname = test_input($_POST["achternaam"]);
+      // check if name only contains letters and whitespace
+      if (!preg_match("/^[a-zA-Z ]*$/",$lname)) {
+        $lnameErr = "Alleen letters en spaties toegestaan";
+      }
+    }
+    
+  if (empty($_POST["emailadres"])) {
+    $emailErr = "Emailadres is verplicht";
+  } else {
+    $email = test_input($_POST["emailadres"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Ongeldig emailadres";
+    }
+  }
+
+  if (empty($_POST["geslacht"])) {
+    $genderErr = "Geslacht is verplicht";
+  } else {
+    $gender = test_input($_POST["geslacht"]);
+  }
+}
+
+// hier opschoning invoer
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+<br>
+<h2>Inschrijfformulier ZZV'92</h2>
+<p><span class="error">* required field</span></p>
 <br>
 <br>
 <br>
-
-<form action="welcome.php" method="post">
+// meldingen komen in het formulier zelf, geen geschakel tussen resultaat en invoer
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
 
 Voornaam: <input type="text" name="voornaam"><br>
-<br>
-<br>
+<span class="error">* <?php echo $fnameErr;?></span>
+<br><br>
 Achternaam: <input type="text" name="achternaam"><br>
-<br>
+<span class="error">* <?php echo $lnameErr;?></span><br>
 <br>
 
 Leeftijd: <input type="text" name="leeftijd"><br>
@@ -31,10 +82,12 @@ Leeftijd: <input type="text" name="leeftijd"><br>
 Geslacht<br>
   <input type="radio" name="geslacht" value="man" checked> Man
   <input type="radio" name="geslacht" value="vrouw"> Vrouw
-  <input type="radio" name="geslacht" value="anders"> Anders
+  <input type="radio" name="geslacht" value="anders"> Anders<br>
+  <span class="error">* <?php echo $genderErr;?></span>
 <br>
 <br>
 Emailadres: <input type="text" name="emailadres"><br>
+<span class="error">* <?php echo $emailErr;?></span>
 <br>
 <br>
 Betaalperiode<br>
@@ -48,13 +101,19 @@ Rekeningnummer: <input type="text" name="rekeningnummer"><br>
 <br>
 <br>
 
-
 <input type="submit" >
 
 </form>
 
-
-</body>
+<?php
+echo "<h2>Your Input:</h2>";
+echo $fname . " " . $lname;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $gender;
+?>
+ </body>
 
 
 </html> 
