fc17245a800c21e34c73241525f7df72c69bf2c5
RomanWiley <coenena21@gmail.com>
2019-01-09 20:01:59 +0100

    bootstrap4 added

--- opdracht04.php
+++ opdracht04.php
@@ -3,12 +3,21 @@
 <head>
 
 <title> Opdracht 04, arrays in PHP </title>
+<!-- de opdracht is om een afbeelding van een ladenkast te maken,
+daarmee wordt dan grafisch het idee van een array weergegeven.
+de verschillende PHP functies van arrays moeten worden weergegeven
+-->
+
+<meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
 </head>
 
 <body>
 
 <?PHP
+echo "Een array is een soort ladenkast met gegevens<br>";
+echo "Een standaard array geeft iedere lade een nummer, beginnend met 0<br>";
 
 $fruit = array("bananen", "appels", "peren", "sinaasappels");
 
@@ -78,6 +87,12 @@ echo "<br><br>";
 
 
 ?>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
