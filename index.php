cd2b70fed2911a28a6e6751dccdf93a601073ee8
RomanWiley <coenena21@gmail.com>
2019-01-09 21:37:25 +0100

    startnavigation added to index.php

--- index.php
+++ index.php
@@ -6,7 +6,19 @@
 <br>
 <br>
 
-<a href="https://zzv92.herokuapp.com/formulier.php"> Inschrijfformulier </a>
+<a href="https://zzv92.herokuapp.com/formulier.php"> <b>Inschrijfformulier Z.Z.V. '92</b> </a><br>
+<br><br>
+<b>Andere opdrachten:</b><br><br>
+<a href="https://zzv92.herokuapp.com/opdracht01.php"> Opdracht 1 </a><br>
+<br>
+<a href="https://zzv92.herokuapp.com/opdracht02.php"> Opdracht 2 </a><br>
+<br>
+<a href="https://zzv92.herokuapp.com/opdracht03.php"> Opdracht 3 </a><br>
+<br>
+<a href="https://zzv92.herokuapp.com/opdracht04.php"> Opdracht 4 (arrays & CSS) </a><br>
+<br>
+
+
 
 
 </body>
